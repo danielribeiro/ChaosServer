1a3d8342fe97c797602625611726ae71bf776786
Daniel Ribeiro <danielrb@pagerduty.com>
2013-04-30 18:00:21 -0700

    chaos server basic capabilities created

--- chaos_server.rb
+++ chaos_server.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
 get '/' do
-  "Chaos server"
+    sleepTime = params[:sleep].to_i
+    statusParam = params[:status]
+    response_body = params[:response_body] || "response_body"
+    repeat = (params[:repeat] || 1).to_i
+    sleep(sleepTime) if sleepTime > 0
+    status statusParam if statusParam
+    response_body * repeat
+end
+
+get '/throw_exception' do
+  raise "an exception"
 end
